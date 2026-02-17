ee271ebd036163e7011939ad91a805d91f3b91df
Krishna75305 <yadavkrishna75305@gmail.com>
2026-02-17 16:40:13 +0530

    initial commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import StatCard from './StatCard';
 
 function About() {
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Navbar from '../componets/Navbar';
-import Hero from '../componets/Hero';
-import Features from '../componets/Features';
-import About from '../componets/About';
-import CTA from '../componets/CTA';
-import Footer from '../componets/Footer';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import About from '../components/About';
+import TestimonialSection from '../components/TestimonialSection';
+import NewsletterSection from '../Newsletter';
+import Footer from '../components/Footer';
 
 function LandingPage() {
   return (
@@ -13,7 +14,8 @@ function LandingPage() {
       <Hero />
       <Features />
       <About />
-      <CTA />
+      <TestimonialSection/>
+       <NewsletterSection />
       <Footer />
     </div>
   );
